81b48fdb5a784519b3b9cd58062e028f9b5aa3ef
Nicolas Hervy <nicolas.hervy@spark-vision.com>
2019-11-19 16:47:59 +0100

    Added wrapper to slider. Added left/right chevron.

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,8 +1,14 @@
 import React, { useRef } from 'react';
 import styled from 'styled-components';
 import { debounce } from 'lodash';
+// eslint-disable-next-line
+import feather, { chevronLeft } from '../../node_modules/feather-icons/dist/feather';
 
-const CardsContainer = styled.nav`
+const ComponentWrapper = styled.nav`
+  position: relative;
+`;
+
+const CardsContainer = styled.div`
   background: orangered;
   font-family: sans-serif;
   scroll-snap-type: x mandatory;
@@ -10,8 +16,9 @@ const CardsContainer = styled.nav`
   -webkit-overflow-scrolling: touch;
   overflow-x: overlay;
   overflow-y: hidden;
+  height: 100%;
   &::-webkit-scrollbar{
-    width: 8px;
+    height: 8px;
   }
   &::-webkit-scrollbar-track {}
   &::-webkit-scrollbar-thumb {
@@ -23,7 +30,6 @@ const CardsContainer = styled.nav`
 const CardSection = styled.section`
   min-width: 100vw;
   height: 100%;
-  box-shadow: inset 0px 0px 0px 2px rgb(189, 14, 14);
   scroll-snap-align: center;
   scroll-snap-stop: always;
   position: relative;
@@ -46,6 +52,33 @@ const CardDiv = styled.div`
   cursor: pointer;
 `;
 
+const ChevronDiv = styled.div.attrs(props => ({
+  style: props.right ? {transform:'rotate(180deg)',left:'unset',right:'0'}: null,
+}))`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  overflow: hidden;
+  &:hover {
+    color: #000;
+    svg {
+    transform: scale(2.3);
+    }
+  }
+  svg {
+    transform: scale(2);
+    transition: 200ms all;
+  }
+`;
+
+
+
 function Slider({ cards, handleSelect, defaultCard, onCardClick }) {
   // const { isScrolled, setScrolled } = useState(false);
   // const getCard = index => {
@@ -58,6 +91,7 @@ function Slider({ cards, handleSelect, defaultCard, onCardClick }) {
   );
   // eslint-disable-next-line
   const [sectionWidth, setSectionWidth] = React.useState(0);
+  const componentWrapper = useRef();
   const sliderContainer = useRef();
   const cardEl = useRef();
 
@@ -67,24 +101,33 @@ function Slider({ cards, handleSelect, defaultCard, onCardClick }) {
       sliderContainer.current.scrollLeft /
         sliderContainer.current.offsetWidth,
     );
-    if (selectedCardnr === 0) isScrollStart();
+    if (selectedCardnr === 0) isScrollBegining();
     else if (selectedCardnr >= cards.length - 1) isScrollEnd();
+    else isScrollMiddle();
     console.log(selectedCardnr + 1);
     handleSelect(cards[selectedCardnr]);
 
-    function isScrollStart() {
-      console.log('Scroll start.');
+    function isScrollBegining() {
+      document.querySelector('.chevron.left').style.display='none';
     }
     function isScrollEnd() {
-      console.log('Scroll end.');
+      document.querySelector('.chevron.left').style.display='none';
+    }
+    function isScrollMiddle() {
+      document.querySelector('.chevron.left').style.display='flex';
     }
   }
 
+  // Do when component starts up.
   React.useEffect(scrollStop, []);
+  // Initiate React Feather icons.
+  React.useEffect(() => feather.replace(),[]);
+  // Store width of one section in preparation for a desktop view.
   React.useEffect(() => {
     setSectionWidth(cardEl.current.offsetWidth);
   }, []);
 
+  // Get called debounced at the trailing end, ie when motion stops.
   const handleScroll = e => scrollStop(e);
 
   const handleOnClick = e => {
@@ -97,17 +140,21 @@ function Slider({ cards, handleSelect, defaultCard, onCardClick }) {
   }
 
   return (
-    <CardsContainer
-      ref={sliderContainer}
-      className="cards-container"
-      onScroll={debounce(handleScroll, 200)}
-    >
-      {cards.map((cardItem, i) => (
-        <CardSection className="card-section" key={i} ref={cardEl} draggable='true' onDragStart={onDragStart}>
-          <CardDiv onClick={handleOnClick}>{cardItem.itemNr}</CardDiv>
-        </CardSection>
-      ))}
-    </CardsContainer>
+    <ComponentWrapper ref={componentWrapper}>
+      <CardsContainer
+        ref={sliderContainer}
+        className="cards-container"
+        onScroll={debounce(handleScroll, 200)}
+      >
+        {cards.map((cardItem, i) => (
+          <CardSection className="card-section" key={i} ref={cardEl} draggable='true' onDragStart={onDragStart}>
+            <CardDiv onClick={handleOnClick}>{cardItem.itemNr}</CardDiv>
+          </CardSection>
+        ))}
+      </CardsContainer>
+      <ChevronDiv left className="chevron left"><i data-feather="chevron-left"></i></ChevronDiv>
+      <ChevronDiv right className="chevron right"><i data-feather="chevron-left"></i></ChevronDiv>
+    </ComponentWrapper>
   );
 }
 
